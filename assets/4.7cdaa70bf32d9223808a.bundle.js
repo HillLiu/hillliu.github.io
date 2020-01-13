ef77be5e810c61952305f722ee845e4f85c95ce2
Hill <hill@kimo.com>
2020-01-14 04:12:58 +0800

    update animate

---
+++ assets/4.7cdaa70bf32d9223808a.bundle.js
@@ -0,0 +1,13 @@
+webpackJsonp([4],{
+
+/***/ 417:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = function() {
+  return new Worker(__webpack_require__.p + "ff65c39a095daa020b69.worker.js");
+};
+
+/***/ })
+
+});
+//# sourceMappingURL=4.7cdaa70bf32d9223808a.bundle.js.map
